846e5bc8ad97f03de37832e0adb4c57e847d8069
Farid Rashidi <farid.rash@gmail.com>
2026-03-23 14:17:35 +0100

    Modified app.py

--- UI/app.py
+++ UI/app.py
@@ -1,11 +1,25 @@
 import streamlit as st
+import requests
+
+API_URL = "https://radio-galaxy-api-783206752653.europe-west1.run.app"
 
 st.title("Radio Galaxy Classifier")
 
-uploaded_file = st.file_uploader(
-    "Upload an image",
-    type=["jpg", "jpeg", "png"]
-)
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
+    st.image(uploaded_file, use_container_width=True)
+
+if uploaded_file is not None and st.button("Predict"):
+    with st.spinner("Sending to API..."):
+        response = requests.post(
+            API_URL,
+            files={"file": uploaded_file.getvalue()}
+        )
+
+    st.write("Status:", response.status_code)
+
+    if response.status_code == 200:
+        st.json(response.json())
+    else:
+        st.error(response.text)
