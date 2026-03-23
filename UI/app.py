bcdce63e181793860bc85b9f78b87d1d1cc78d9e
Farid Rashidi <farid.rash@gmail.com>
2026-03-23 14:35:37 +0100

    the final ver of  app.py

--- UI/app.py
+++ UI/app.py
@@ -1,25 +1,48 @@
 import streamlit as st
 import requests
 
-API_URL = "https://radio-galaxy-api-783206752653.europe-west1.run.app"
+API_URL = "https://radio-galaxy-api-783206752653.europe-west1.run.app/"
 
-st.title("Radio Galaxy Classifier")
+st.set_page_config(page_title="Radio Galaxy Classifier", layout="centered")
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+st.title("🔭 Radio Galaxy Classifier")
+st.markdown("Upload a radio galaxy image to classify its morphology")
+
+uploaded_file = st.file_uploader(
+    "Upload image",
+    type=["jpg", "jpeg", "png"]
+)
 
 if uploaded_file is not None:
     st.image(uploaded_file, use_container_width=True)
 
-if uploaded_file is not None and st.button("Predict"):
-    with st.spinner("Sending to API..."):
-        response = requests.post(
-            API_URL,
-            files={"file": uploaded_file.getvalue()}
-        )
+    if st.button("Predict"):
+        with st.spinner("Analyzing..."):
+            image_bytes = uploaded_file.getvalue()
 
-    st.write("Status:", response.status_code)
+            # detect correct content type
+            filename = uploaded_file.name.lower()
+            content_type = "image/png" if filename.endswith(".png") else "image/jpeg"
 
-    if response.status_code == 200:
-        st.json(response.json())
-    else:
-        st.error(response.text)
+            response = requests.post(
+                API_URL,
+                data=image_bytes,
+                headers={"Content-Type": content_type}
+            )
+
+        if response.status_code == 200:
+            result = response.json()
+
+            label = result["predicted_label"]
+            probs = result["probabilities"]
+
+            confidence = max(probs)
+
+            st.success(f"Prediction: {label}")
+            st.metric("Confidence", f"{confidence*100:.2f}%")
+
+            st.subheader("Class Probabilities")
+            st.bar_chart(probs)
+
+        else:
+            st.error("Prediction failed. Try another image.")
